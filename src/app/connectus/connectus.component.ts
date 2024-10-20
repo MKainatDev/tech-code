e842b3998ac91101b80d8c9bda4b19bd88dc8ece
MKainatDev <mehwishsoftengr@gmail.com>
2024-10-19 23:12:03 -0700

    tech code

---
+++ src/app/connectus/connectus.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connectus',
+  standalone: true,
+  imports: [],
+  templateUrl: './connectus.component.html',
+  styleUrl: './connectus.component.css'
+})
+export class ConnectusComponent {
+
+}
