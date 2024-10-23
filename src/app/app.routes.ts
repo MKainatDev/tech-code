5a64f563edf76a8adde4102454899b499a6af795
MKainatDev <mehwishsoftengr@gmail.com>
2024-10-23 09:51:27 -0700

    rouring

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ProductsComponent } from './products/products.component';
+import { WhychooseusComponent } from './whychooseus/whychooseus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+
+export const routes: Routes = [
+    {path:'PRODUCTS',component:ProductsComponent},
+    {path:'WHYCHOOSEUS',component:WhychooseusComponent},
+    {path:'CONTACTUS',component:ContactusComponent},
+    {path:'ABOUTUS',component:AboutUsComponent}
+];
