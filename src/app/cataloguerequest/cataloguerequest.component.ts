c6c1a326420cdcaf39938b63d962afaaeab0fbf8
MKainatDev <mehwishsoftengr@gmail.com>
2024-10-28 12:33:39 -0700

    routing form

--- src/app/cataloguerequest/cataloguerequest.component.ts
+++ src/app/cataloguerequest/cataloguerequest.component.ts
@@ -1,14 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, NgForm, Validators } from '@angular/forms';
 
 
 @Component({
   selector: 'app-cataloguerequest',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './cataloguerequest.component.html',
   styleUrl: './cataloguerequest.component.css'
 })
 export class CataloguerequestComponent  {
+regdata:FormGroup;
+constructor(private _formBuilder: FormBuilder){
+  this.regdata = this._formBuilder.group({
+    name:['',Validators.required],
+    email:['',Validators.compose,([Validators.required,Validators.email])],
+    contact:['',Validators.required],
+    message:['',Validators.required],
+  })
+}
+ngOnInit():void{
+  this.regdata=new FormGroup({
+    name:new FormControl(),
+    email:new FormControl(),
+    contact:new FormControl(),
+    message:new FormControl(),
+  })
+}
 
+
+send(regdata:NgForm){
+  console.log(regdata.value)
+  console.log(regdata.valid)
+  console.log(this.regdata.get('email')?.value)
+}
 
 }
